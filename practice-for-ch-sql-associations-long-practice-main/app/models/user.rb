1eab3702bdcd30109d3add3e7e93477ebf50d6fd
David Lai <pf.davidlai@gmail.com>
2023-03-02 17:06:52 -0800

    finished

--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -1,2 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+
+    
+
+    
+    has_many :Enrollments,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_courses,
+        primary_key:  :id,
+        foreign_key: :course_id,
+        through: :Enrollments,
+        source: :Course
+
 end
